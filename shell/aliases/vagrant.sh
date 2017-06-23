2c143277c780dbde92ce412fb5228189a9d7c85f
Raul Garcia <satanin@gmail.com>
2017-06-23 13:08:29 +0200

    fix alias

--- shell/aliases/vagrant.sh
+++ shell/aliases/vagrant.sh
@@ -13,7 +13,7 @@ alias vreload="vagrant reload"
 # vagrant environment aliases
 ###############################################################################
 alias b='babushka'
-alias payup='babushka pt:payshcool.up'
+alias payup='babushka pt:payschool.up'
 alias coreup='babushka pt:core.up'
 alias operup='babushka pt:operations.up'
 alias dashrup='babushka pt:dashboards.up'
