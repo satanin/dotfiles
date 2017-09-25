f153198c41655828d777665b3487b4558c6ab50f
Raul Garcia <satanin@gmail.com>
2017-09-25 15:48:19 +0200

    add git completion with aliases

--- shell/bash_profile.sh
+++ shell/bash_profile.sh
@@ -9,7 +9,16 @@ fi
 ###########################################
 if [ -f ~/.git-completion.bash ]; then
     source ~/.git-completion.bash
+    # Add git completion to aliases
+   __git_complete ga _git_add
+   __git_complete gs _git_status
+   __git_complete gb _git_branch
+   __git_complete gco _git_checkout
+   __git_complete gm _git_merge
+   __git_complete ggpull _git_pull
+   __git_complete ggpush _git_push
 fi
+
 ###########################################
 # Load aliases from bash_aliases if exists
 ###########################################
@@ -29,5 +38,3 @@ if [ "${USER}" == "raulgarciaruiz" ]; then
 else
   . /usr/share/autojump/autojump.sh
 fi
-
-
