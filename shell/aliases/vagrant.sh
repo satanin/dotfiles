44dda2d7d5b6bbe9c1fb7ee3fa5493e761462ef4
Raul Garcia <satanin@gmail.com>
2018-08-28 12:56:00 +0200

    Remove editor config and add babushka aliases

--- shell/aliases/vagrant.sh
+++ shell/aliases/vagrant.sh
@@ -8,13 +8,3 @@ alias vup='vu --provision'
 alias vssh='vagrant ssh'
 alias vhalt="vagrant halt"
 alias vreload="vagrant reload"
-
-###############################################################################
-# vagrant environment aliases
-###############################################################################
-alias b='babushka'
-alias payup='babushka pt:payschool.up'
-alias coreup='babushka pt:core.up'
-alias operup='babushka pt:operations.up'
-alias dashrup='babushka pt:dashboards.up'
-
