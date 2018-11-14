9d00eb544caf8d5788fa5bedc750032d12320d0d
Raul Garcia <satanin@gmail.com>
2018-11-14 08:57:48 +0100

    add tmux alias

--- shell/aliases/tmux.sh
+++ shell/aliases/tmux.sh
@@ -1,3 +1,4 @@
 ########### TMUX ALIASES #############
 alias flywire='tmux new -s flywire||tmux a -t flywire'
+alias easy='tmux new -s flywire||tmux a -t easy'
 ######################################
