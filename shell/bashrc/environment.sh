935d4e439601c29a6b01571eaa741ccba4e9172c
Raul Garcia <satanin@gmail.com>
2020-09-14 17:17:47 +0200

    Update fresh

--- shell/bashrc/environment.sh
+++ shell/bashrc/environment.sh
@@ -14,3 +14,9 @@ export PATH=$HOME/.rbenv/shims:$PATH
 # EDITOR
 ###########################
 export EDITOR='vim'
+
+###########################################
+# Docker CLI
+###########################################
+export COMPOSE_DOCKER_CLI_BUILD=1
+export DOCKER_BUILDKIT=1
