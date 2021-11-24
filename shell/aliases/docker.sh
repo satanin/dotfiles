8b1fa1a881a94a3d3d9822be547dd934a0c1a95a
Raul Garcia <satanin@gmail.com>
2021-11-24 13:05:15 +0100

    update tmux conf gitignore and docker aliases

--- shell/aliases/docker.sh
+++ shell/aliases/docker.sh
@@ -1,6 +1,6 @@
-  ###############################################################################
-  # docker environment aliases
-  ###############################################################################
+###############################################################################
+# docker environment aliases
+###############################################################################
 
-alias stop_containers='docker ps | awk '{if (NR!=1) {print $1}}' | xargs docker stop'
-alias remove_images='docker images | grep none | awk '{ print $3}' | xargs docker image rm'
+alias stop_containers="docker ps | awk '{if (NR!=1) {print $1}}' | xargs docker stop"
+alias remove_images="docker images | grep none | awk '{ print $3}' | xargs docker image rm"
